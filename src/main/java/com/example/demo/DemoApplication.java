34bc0c71c25b4187bc0a83f8e9c882ea9f3a9a98
hytlilu <hyt_amy@163.com>
2018-02-22 15:39:18 +0800

    修改banner

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.Controller.HelloController;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * SpringBoot项目启动类
@@ -11,5 +14,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        //启动时关闭Banner
+//        SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
+//        builder.bannerMode(Banner.Mode.OFF).run(args);
+
     }
 }
